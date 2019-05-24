04ef988920f19444177432b806f10dc4724c5037
Gastón Rossi <cf18gaston.rossi@iesjoandaustria.org>
2019-05-24 18:43:50 +0200

    Ejercicio 17 acabado. Eliminación de la variable bonificacions, implementación del metodo bonificacionsTotals()

--- src/Client.java
+++ src/Client.java
@@ -55,12 +55,10 @@ public class Client {
     }
 
     public String informe() {
-        int bonificacions = 0;
         String resultat = "Informe de lloguers del client " +
                 getNom() +
                 " (" + getNif() + ")\n";
         for (Lloguer lloguer: lloguers) {
-            bonificacions += lloguer.bonificacions();
             // composa els resultats d'aquest lloguer
             resultat += "\t" +
                     lloguer.getVehicle().getMarca() +
@@ -71,7 +69,7 @@ public class Client {
 
         // afegeix informació final
         resultat += "Import a pagar: " + importTotal() + "€\n" +
-                "Punts guanyats: " + bonificacions + "\n";
+                "Punts guanyats: " + bonificacionsTotals() + "\n";
         return resultat;
     }
 
@@ -81,6 +79,14 @@ public class Client {
             total += lloguer.quantitat() * 30;
         }
         return total;
+    }
+
+    public int bonificacionsTotals() {
+        int bonificacions = 0;
+        for (Lloguer lloguer: lloguers) {
+            bonificacions += lloguer.bonificacions();
+        }
+        return bonificacions;
     }
 
     public int size() {
